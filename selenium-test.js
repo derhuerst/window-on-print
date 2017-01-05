a9412180cf878e8b12d1cb71b41cbf6f19506166
Jannis R <mail@jannisr.de>
2017-01-06 00:21:02 +0100

    Firefox, Safari :sparkles:

--- selenium-test.js
+++ selenium-test.js
@@ -6,8 +6,10 @@ const assert = require('assert')
 
 const user = process.env.SAUCE_USERNAME
 const key = process.env.SAUCE_ACCESS_KEY
-const job = process.env.TRAVIS_JOB_NUMBER
 const build = process.env.TRAVIS_BUILD_NUMBER
+const job = process.env.TRAVIS_JOB_NUMBER
+const browser = process.env.BROWSER
+const platform = process.env.PLATFORM
 
 
 
@@ -15,9 +17,9 @@ const browser = webdriver.remote({
 	user, key, host: 'localhost', port: 4445,
 	logLevel: 'silent',
 	desiredCapabilities: {
-		'tunnel-identifier': job, build, name: 'selenium-test',
-		browserName: 'chrome',
-		platform: 'Windows 10',
+		build, 'tunnel-identifier': job, name: 'selenium-test',
+		browserName: browser,
+		platform,
 		recordScreenshots: false,
 		chromeOptions: {args: ['--kiosk', '--kiosk-printing']}
 	}
