830cdd47dc2854e6bb0ab424a443fc5c7e9aec90
Jannis R <mail@jannisr.de>
2017-01-05 21:33:22 +0100

    does this even run?

--- selenium-test.js
+++ selenium-test.js
@@ -1,27 +1,6 @@
 'use strict'
 
-const webdriver = require('webdriverio')
-
 const user = process.env.SAUCE_USERNAME
 const key = process.env.SAUCE_ACCESS_KEY
+
 console.log('sauce lab', user.slice(0, 3), key.slice(0, 3))
-const job = process.env.TRAVIS_JOB_NUMBER
-const build = process.env.TRAVIS_BUILD_NUMBER
-
-
-
-webdriver.remote({
-	user, key,
-	host: 'ondemand.saucelabs.com',
-	desiredCapabilities: {
-		'tunnel-identifier': job, build, name: 'selenium-test',
-		browserName: 'chrome',
-		platform: 'Windows 10',
-		recordScreenshots: false
-	}
-})
-.init()
-.url(`http://${user}:${key}@localhost:4445/wd/hub`)
-.getTitle()
-.then((title) => console.log('page title', title))
-.end()
